06f2a152e4e1e2af95c91c45b37338627c48b52f
Samuel <samuelkumar721@gmail.com>
2024-09-13 18:11:17 +0530

    initializing

---
+++ app.js
@@ -0,0 +1,10 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server is listening to port 8080");
+});
